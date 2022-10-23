4c9a177d3966a07a5a76736c6fa4f0fe460f936b
Nesterenko Sophia <nykolay76@yandex.ru>
2022-10-23 18:33:24 +0200

    add gitignore & ruleset

---
+++ Module3HW5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module3HW5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
