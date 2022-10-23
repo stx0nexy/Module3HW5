2912123d8b5bf546ee8a47cd4d800a1c56f3e625
Nesterenko Sophia <nykolay76@yandex.ru>
2022-10-23 18:53:08 +0200

    add program

---
+++ Module3HW5/Methods.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Module3HW5
+{
+    public class Methods
+    {
+        public async Task<string> GetAllTextAsync()
+        {
+            string hello = await GetHelloFileAsync();
+            string world = await GetWorldFileAsync();
+            return $"{hello} {world}";
+        }
+
+        private async Task<string> GetHelloFileAsync()
+        {
+            string hello = await File.ReadAllTextAsync("hello.txt");
+            return hello;
+        }
+
+        private async Task<string> GetWorldFileAsync()
+        {
+            string world = await File.ReadAllTextAsync("world.txt");
+            return world;
+        }
+    }
+}
